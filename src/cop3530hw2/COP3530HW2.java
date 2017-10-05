964222279ae858f40363bafdbb3849736000ae9f
antunez0425 <antunez0425@users.noreply.github.com>
2017-10-05 16:06:02 -0400

    WIP boolean subset

---
+++ src/cop3530hw2/COP3530HW2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cop3530hw2;
+
+/**
+ *
+ * @author aantu009
+ */
+public class COP3530HW2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
