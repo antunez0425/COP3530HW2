52abfe55ab6df23653ca85031e0fd632772c9ecd
antunez0425 <antunez0425@users.noreply.github.com>
2017-10-05 16:39:12 -0400

    more work on subset WIP

--- src/cop3530hw2/COP3530HW2.java
+++ src/cop3530hw2/COP3530HW2.java
@@ -5,17 +5,50 @@
  */
 package cop3530hw2;
 
+import java.util.List;
+import java.util.ListIterator;
+
 /**
  *
  * @author aantu009
  */
-public class COP3530HW2 {
+public class COP3530HW2
+{
+    //WIP
+    public static <AnyType extends Comparable<? super AnyType>>
+            boolean subset(List<AnyType> L1, List<AnyType> L2)
+    {
+        boolean isSubset = true;
+        AnyType itemL1;
+        AnyType itemL2;
+        
+        ListIterator<AnyType> iterL1 = L1.listIterator();
+        ListIterator<AnyType> iterL2 = L2.listIterator();
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+        // get first item in each list
+        if (iterL1.hasNext() && iterL2.hasNext())
+        {
+            itemL1 = iterL1.next();
+            itemL2 = iterL2.next();
+        }
+        
+        for(int i = 0; i < L1.size(); i++)
+        {
+            for(int j = 0; i < L2.size(); i++)
+            {
+                if(itemL1.compareTo(itemL2) == 0)
+                {
+                    
+                }
+            }
+        }
+        
+        return isSubset;
+    }
+
+    public static void main(String[] args)
+    {
         // TODO code application logic here
     }
-    
+
 }
